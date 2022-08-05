e935b552dab76c65790022555597552e6e3d96d6
Santiago Escuder <Santiago_56@hotmail.com>
2022-08-05 11:37:31 -0300

    Updates

---
+++ precache-manifest.ba9b230282f3444db7054d22986b7e5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c153776567ecda67543c",
+    "url": "/APPlatzi/static/css/main.ff3e20ed.chunk.css"
+  },
+  {
+    "revision": "c153776567ecda67543c",
+    "url": "/APPlatzi/static/js/main.c1537765.chunk.js"
+  },
+  {
+    "revision": "bee36d96ecf68392c5e3",
+    "url": "/APPlatzi/static/js/1.bee36d96.chunk.js"
+  },
+  {
+    "revision": "ce95c2de4a37766b3b6b",
+    "url": "/APPlatzi/static/js/runtime~main.ce95c2de.js"
+  },
+  {
+    "revision": "58c36f548e0a152a18d51efd87783439",
+    "url": "/APPlatzi/static/media/check-svgrepo-com.58c36f54.svg"
+  },
+  {
+    "revision": "1cec76f7c9f0241cd6982c3f8d3cd628",
+    "url": "/APPlatzi/static/media/delete-svgrepo-com.1cec76f7.svg"
+  },
+  {
+    "revision": "16b7f50d212be9af6c2ce86199b33649",
+    "url": "/APPlatzi/index.html"
+  }
+];
